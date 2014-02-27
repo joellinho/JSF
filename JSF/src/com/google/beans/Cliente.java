a6b84e56e98ae5677d8310d70bc5c1931d681f78
jchoy1088 <jonathan.choy@hotmail.com>
2014-02-27 15:17:28 -0500

    No sé que hice... :(

--- JSF/src/com/google/beans/Cliente.java
+++ JSF/src/com/google/beans/Cliente.java
@@ -4,13 +4,7 @@ import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name="cliente")
 public class Cliente {
-<<<<<<< HEAD
-	
-	// Connect Successfull
-	
-=======
-	// Testing for branch
->>>>>>> origin/Gerson-Silva
+
 	private String Nombre="Luyito Gay";
 
 	public String getNombre() {
